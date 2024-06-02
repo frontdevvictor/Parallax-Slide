997fcc914733675f6d144aba674d4d39d045c8ae
Victor Hugo <frontdevvictor@gmail.com>
2024-06-01 22:19:04 -0300

    Atualizando!

--- index.js
+++ index.js
@@ -1,28 +1,32 @@
 const options = {
-    acessibility: true,
+    accessibility: true,
     prevNextButtons: true,
     pageDots: true,
     setGallerySize: false,
+    wrapAround: true, // Adiciona wrap around para transições contínuas
+    freeScroll: false, // Desativa o free scroll para melhor controle
+    friction: 0.8, // Ajusta a fricção para suavizar a transição
+    selectedAttraction: 0.025, // Ajusta a atração para suavizar a transição
     arrowShape: {
         x0: 1,
         x1: 58,
+        y1: 50,
         x2: 62,
-        x3: 55,
         y2: 48,
         x3: 18
     }
 };
 
-// Function to set background position forr slides
+// Function to set background position for slides
 function setBgPosition(slide, index) {
     const x = -(slide.target + flkty.x) / 3;
-    slides(index).style.backgroundPosition = '${x}px'
+    slides[index].style.backgroundPosition = `${x}px`;
 }
 
 // Slides initialization 
-const carousel = document.querySelector('[carousel]')
+const carousel = document.querySelector('.carousel');
 const slides = Array.from(document.getElementsByClassName('carousel-cell'));
-const flkty = new flickity(carousel, options);
+const flkty = new Flickity(carousel, options);
 
 // Event listener using bg position 
 flkty.on('scroll', () => {
